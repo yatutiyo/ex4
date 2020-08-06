21b65f77e693033bf8640c446dcf6aca5678b02e
Azuma <s1270107@u-aizu.ac.jp>
2020-08-06 20:05:55 +0900

    HoT: Initial revision

---
+++ headtail.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+    int i,result[3],headnum=0,tailnum=0;
+    srand((unsigned int)time(NULL));
+
+    printf("Tossing a coin...\n");
+
+    for(i=0;i<3;i++){
+        result[i] = rand() % 2;
+
+        if(result[i]==0) {
+            printf("Round %d: Heads\n",i+1);
+            headnum++;
+        }
+
+        if(result[i]==1) {
+            printf("Round %d: Tails\n",i+1);
+            tailnum++;
+        }
+    }
+
+    printf("Heads: %d, Tails: %d\n",headnum,tailnum);
+    return 0;
+}
